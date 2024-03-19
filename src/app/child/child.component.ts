f9b6d2a60dff7632090cf52ae89444a7354f0db8
Praween Apichaiyingyod <Praween.Apichaiyingyod@krungsri.com>
2024-03-19 15:00:07 +0700

    migrate to signal

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,11 +1,11 @@
-import { CommonModule } from "@angular/common";
-import { Component } from "@angular/core";
+import { CommonModule, NgFor } from "@angular/common";
+import { Component, EventEmitter, Input, Output } from "@angular/core";
 import { DataService } from "../data.service";
 
 @Component({
   selector: "app-child",
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgFor],
   template: `
     <input type="text" #data />
     <button (click)="addNewMessage(data.value); data.value = ''">Update</button>
