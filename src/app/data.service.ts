b751f49639612074ad750eb8ec68628472a25a53
Praween Apichaiyingyod <Praween.Apichaiyingyod@krungsri.com>
2024-03-19 14:28:47 +0700

    edit parent

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,11 @@ import { Injectable } from '@angular/core';
 export class DataService {
 
   constructor() { }
+
+  private messageSubject = new BehaviorSubject<string[]>([]);
+  messages$ = this.messageSubject.asObservable();
+
+  addNewMessage(newMessage: string) {
+    this.messageSubject.next([...this.messageSubject.value, newMessage]);
+  }
 }
